447922a97675b1fb85e35ffcb4492aa0f6dfa9b8
anbalt <anastassija.solomko@gmail.com>
2022-12-13 22:20:31 +0300

    added Files and .gitignore

---
+++ src/by/itacademy/hw2/Task2.java
@@ -0,0 +1,15 @@
+package by.itacademy.hw2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 10;
+        int s = a + b;
+        int m = a * b;
+
+        System.out.println(sum(s, m));
+    }
+    private static int sum(int s, int m) {
+        return s + m;
+    }
+}
