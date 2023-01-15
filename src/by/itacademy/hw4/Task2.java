c04210723ebf52fcbeb91633cb2db3f539219f49
Nastya Solomko <118686532+anbalt@users.noreply.github.com>
2023-01-15 19:06:12 +0300

    Update Task2.java

--- src/by/itacademy/hw4/Task2.java
+++ src/by/itacademy/hw4/Task2.java
@@ -6,15 +6,25 @@ public class Task2 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Add a value");
-
+        
+        System.out.println("Add a positive value to sum up its digits:");
         int n = scanner.nextInt();
-        System.out.println("The sum of the numbers:"  + sumUpDigits(n));
+        System.out.println("The sum of the positive numbers:"  + sumUpPositiveDigits(n));
 
+        System.out.println("Add a negative value to sum up its digits:");
+        int n2 = scanner.nextInt();
+        System.out.println("The sum of the negative numbers:"  + sumUpNegativeDigits(n2));
+
+        System.out.println("Add a positive value to multiply its digits:");
         int m = scanner.nextInt();
-        System.out.println("The multiplication of the numbers:" + multiplyDigits(m));
+        System.out.println("The multiplication of the positive numbers:" + multiplyPositiveDigits(m));
+
+        System.out.println("Add a negative value to multiply  its digits:");
+        int m2 = scanner.nextInt();
+        System.out.println("The multiplication of the negative numbers:" + multiplyNegativeDigits(m2));
     }
-    public static int sumUpDigits(int n) {
+    
+    public static int sumUpPositiveDigits(int n) {
         int sum = 0;
         while (n != 0) {
             sum += (n % 10);
@@ -23,11 +33,29 @@ public class Task2 {
         return sum;
     }
 
-    public static int multiplyDigits(int m) {
+    public static int sumUpNegativeDigits(int n2) {
+        int sum = 0;
+        while (n2 != 0) {
+            sum += (n2 % 10);
+            n2 /= 10;
+        }
+        return sum;
+    }
+    
+    public static int multiplyPositiveDigits(int m) {
         int mul = 1;
-        while (m != 0) {
+        while (m > 0) {
             mul *= (m % 10);
             m /= 10;
+        }
+        return mul;
+    }
+    
+    public static int multiplyNegativeDigits(int m2) {
+        int mul = -1;
+        while (m2 < 0) {
+            mul *= (m2 % 10);
+            m2 /= 10;
         }
         return mul;
     }
