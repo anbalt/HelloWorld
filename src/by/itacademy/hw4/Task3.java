5f0032acebce672aef4e492db62d29429ec84f9e
Nastya Solomko <118686532+anbalt@users.noreply.github.com>
2023-01-15 19:08:35 +0300

    Update Task3.java

--- src/by/itacademy/hw4/Task3.java
+++ src/by/itacademy/hw4/Task3.java
@@ -10,7 +10,7 @@ public class Task3 {
         int n = scanner.nextInt();
         int result = 1;
         for (int i = 1; i <= n; i++) {
-            result = result * i;
+            result *= i;
         }
         System.out.println(result);
     }
