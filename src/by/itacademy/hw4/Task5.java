fcefaa3b29f89405c3c76920bd29ff80a01992ad
Nastya Solomko <118686532+anbalt@users.noreply.github.com>
2023-01-16 23:28:44 +0300

    Update Task5.java

--- src/by/itacademy/hw4/Task5.java
+++ src/by/itacademy/hw4/Task5.java
@@ -3,19 +3,35 @@ package by.itacademy.hw4;
 public class Task5 {
     public static void main(String[] args) {
 
-        int[] array = {6, 39, 7, 3, 12, 5, 1, 10};
+        System.out.println("Enter the array size:");
 
-        int n = array.length;
-        int minIndex = 0;
-        int minValue = array[0];
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue) {
-                minValue = array[i];
-                if (i % 2 != 0);
-                minIndex = i;
+        int size;
+        while ((size = scanner.nextInt()) <= 0) {
+            System.out.println("Error! The array with the entered size can't exist! Enter a value > 0.");
+        }
+
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(50);
+            System.out.println(array[i]);
+        }
+
+
+        int min = array[0];
+        int temp = 0;
+
+
+        for (int i = 0; i < size; i++) {
+            if (i % 2 != 0) {
+                if ((temp = array[i]) < min) {
+                    min = temp;
+                }
             }
         }
-        System.out.println("Minimal element with the odd index: " + minIndex);
+        System.out.println("The min element with the odd index is " + min);
     }
 }
