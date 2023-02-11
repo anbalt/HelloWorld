2cd2052764ccdd32669140733b3f3317ecc21d50
Nastya Solomko <118686532+anbalt@users.noreply.github.com>
2023-02-11 23:14:14 +0300

    Update myBouquet.java

--- src/by/itacademy/hw7/Task4/myBouquet.java
+++ src/by/itacademy/hw7/Task4/myBouquet.java
@@ -1,6 +1,6 @@
 package src.by.itacademy.hw7.Task4;
 
 public interface myBouquet {
-    public void addFlower(Flower flower);
-    public int getBouquetPrice();
+    void addFlower(Flower flower);
+    int getBouquetPrice();
 }
